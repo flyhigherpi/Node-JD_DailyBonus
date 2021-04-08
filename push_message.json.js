a2222e5a94456ec96eec5a845a0ef496ea6fbc2b
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-04-08 23:18:34 +0800

    U

---
+++ push_message.json.js
@@ -0,0 +1,4 @@
+{
+"content1":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
+"content2":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【PoiCloud--月付便宜、丰俭由人】</a></marquee>"
+}
