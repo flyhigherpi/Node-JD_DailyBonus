501fd801bb6badab0ce9dddad257e5120e160533
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-04-14 23:47:57 +0800

    U

--- push_message.json.js
+++ push_message.json.js
@@ -1,5 +1,6 @@
 {
 "content1":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
 "content2":"<em style='color: gold;'>签到0.0.3已上传，支持三账号以上签到</em>",
-"content3":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【PoiCloud--月付便宜、丰俭由人】</a></marquee>"
+"content3":"<em style='color: gold;'>内核为2.6不一定能使用，<a href='https://github.com/Entware/Entware/issues/233' target='_blank'>2.6安装帮助</a></em>",
+"content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【PoiCloud--月付便宜、丰俭由人】</a></marquee>"
 }
