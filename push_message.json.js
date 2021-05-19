c775f7d3c8cda7c3d34740c7edee839eee21c889
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-05-19 11:05:10 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,6 +1,6 @@
 {
 "content1":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
-"content2":"<em style='color: gold;'>签到0.0.3已上传，支持三账号以上签到</em>",
-"content3":"<em style='color: gold;'>内核为2.6不一定能使用，<a href='https://github.com/Entware/Entware/issues/233' target='_blank'>2.6安装帮助</a></em>",
+"content2":"<em style='color: gold;'>内核为2.6不能使用</em>",
+"content3":"<em style='color: gold;'>【推】华为智慧屏代理（价钱优势，全国可发）--石头哥，QQ：20052795。好友暗号：【满地星光】</em>",
 "content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://poicloud.icu/auth/register?code=K36yIuUM1aQePNOpGVTeBrxY0hhN2aGU' target='_blank'>【PoiCloud--月付便宜、丰俭由人】</a></marquee>"
 }
