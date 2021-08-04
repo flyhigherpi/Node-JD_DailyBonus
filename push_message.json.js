b18ab97620dc7649a56b2315357fa81a8705aaca
Fly|HigheR|Pi <67776691+flyhigherpi@users.noreply.github.com>
2021-08-04 12:20:37 +0800

    Y

--- push_message.json.js
+++ push_message.json.js
@@ -1,4 +1,4 @@
 {
 "content1":"<em style='color: gold;'>交流频道：<a href='https://t.me/meilinchajian' target='_blank'>Merlinclash</a></em>",
-"content2":"<em style='color: gold;'>内核为2.6不能使用</em>",
+"content2":"<em style='color: gold;'>内核为2.6不能使用</em>"
 }
